730a3b8e6370ddd388e36844c2c30ad573737926
NuriYildiz <26silentdancer26@gmail.com>
2021-02-13 17:19:39 +0200

    Baby Emin

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Heloo world baby Maria");
+        System.out.println("Hello world baby Emin");
     }
 }
