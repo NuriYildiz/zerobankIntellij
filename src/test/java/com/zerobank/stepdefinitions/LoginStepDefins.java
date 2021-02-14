4f346c4c59ecd28337116cce5de55bafe1a35f7a
NuriYildiz <26silentdancer26@gmail.com>
2021-02-14 20:26:46 +0200

    zero v1

---
+++ src/test/java/com/zerobank/stepdefinitions/LoginStepDefins.java
@@ -0,0 +1,39 @@
+package com.zerobank.stepdefinitions;
+
+import com.zerobank.pages.LoginPage;
+import com.zerobank.utilities.BrowserUtils;
+import com.zerobank.utilities.ConfigurationReader;
+import com.zerobank.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginStepDefins {
+
+    @Given("the user is on the login page")
+    public void the_user_is_on_the_login_page() throws InterruptedException {
+        String url = ConfigurationReader.get("url");
+        //WebDriver driver = Driver.get();
+        Driver.get().get(url);
+    }
+
+    @When("the user enters the username")
+    public void the_user_enters_the_driver_information() throws InterruptedException {
+        String username = ConfigurationReader.get("driver_username");
+        String password = ConfigurationReader.get("driver_password");
+
+        LoginPage loginPage = new LoginPage();
+        loginPage.login(username,password);
+    }
+
+    @Then("the user should be able to login")
+    public void the_user_should_be_able_to_login() throws InterruptedException {
+        BrowserUtils.waitFor(3);
+        String actualTitle = Driver.get().getTitle();
+        Assert.assertEquals("Dashboard",actualTitle);
+
+    }
+}
